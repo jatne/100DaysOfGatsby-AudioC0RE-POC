0930062459572c1d8a0834e21d88c320e43e7c98
Paweł Malok <malok.p@mpcreation.net>
2021-01-24 23:09:58 +0100

    fun with gatsby-image and contentful

--- src/pages/location/{contentfulLocation.slug}.js
+++ src/pages/location/{contentfulLocation.slug}.js
@@ -1,12 +1,16 @@
 import { graphql } from 'gatsby';
 import React from 'react';
 import Layout from '../../components/Layout';
+import Img from 'gatsby-image';
 
 export default function Component({data}) {
-  const {name} = data.contentfulLocation;
+  const {name, hero} = data.contentfulLocation;
+
+  console.log(hero)
 
   return (
     <Layout>
+      {hero?.fluid && <Img fluid={hero.fluid} />}
       <h1>{name}</h1>
     </Layout>
   );
@@ -16,6 +20,11 @@ export const query = graphql`
   query($id: String) {
     contentfulLocation(id: { eq: $id }) {
       name
+      hero {
+        fluid(maxWidth: 1680) {
+          ...GatsbyContentfulFluid
+        }
+      }
     }
   }
 `
